594c17d461fb6d77c57d101b47396c3357429273
Gregory Man <man.gregory@gmail.com>
2009-11-09 23:50:40 +0200

    Use ginger to test with multiple rails versions. Also removed bundled rails.

---
+++ spec/ginger_scenarios.rb
@@ -0,0 +1,26 @@
+require 'ginger'
+
+def create_scenario(version)
+  scenario = Ginger::Scenario.new
+  
+  scenario[/^rails$/]    = version
+  scenario[/^active_?support$/]    = version
+  scenario[/^active_?support$/]    = version
+  scenario[/^active_?record$/]     = version
+  scenario[/^action_?pack$/]       = version
+  scenario[/^action_?controller$/] = version
+  scenario
+end
+
+Ginger.configure do |config|
+  config.aliases["active_record"] = "activerecord"
+  config.aliases["active_support"] = "activesupport"
+  config.aliases["action_controller"] = "actionpack"
+
+  # config.scenarios << create_scenario("2.0.2")
+  # config.scenarios << create_scenario("2.1.2")
+  # config.scenarios << create_scenario("2.2.2")
+  # config.scenarios << create_scenario("2.3.2")
+  config.scenarios << create_scenario("2.3.3")
+  config.scenarios << create_scenario("2.3.4")
+end
