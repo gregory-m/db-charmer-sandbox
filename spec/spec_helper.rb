7cc0522818506e7d92a1acb3f006ef1d53757816
Gregory Man <man.gregory@gmail.com>
2009-11-10 00:52:07 +0200

    Don't require Ginger in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,6 @@
 # This file is copied to ~/spec when you run 'ruby script/generate rspec'
 # from the project root directory.
 ENV["RAILS_ENV"] = 'test'
-require 'rubygems'
-require 'ginger'
 require File.dirname(__FILE__) + "/../config/environment" unless defined?(RAILS_ROOT)
 require 'spec/autorun'
 require 'spec/rails'
